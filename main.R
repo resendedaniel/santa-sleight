5363bada787c10c0b790b7aa67c5c5a04f826717
Daniel Resende <dresende@verios.com.br>
2015-12-26 22:34:25 -0200

    verbose

--- main.R
+++ main.R
@@ -44,7 +44,7 @@ while(any(!data$picked)) {
     slope <- (clusteringTime$t[currCluster] - clusteringTime$t[currCluster - 5]) / (clusteringTime$x[currCluster] - clusteringTime$x[currCluster - 5])
     elt <- clusteringTime$t[currCluster]
     eta <- slope * (1 - clusteringTime$x[currCluster])
-    cat(round(proc.time() - t1, 2)[3], "secs", "|", round(completed * 100, 1), "% completed", " | ",
+    cat(round(proc.time() - t1, 2)[3], "s", "|", round(completed * 100, 1), "%", " |",
         "Elapsed:", round(elt) / 60, "m |\n",
         "Remaining:", round(eta) / 60, "m |\n",
         "Total:", round(elt + eta) / 60, "m |\n")
